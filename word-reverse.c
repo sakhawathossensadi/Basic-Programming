59416e78ec404e1a94b24e8de87f1dd7c078ca9a
sakhawathossensadi <newtonss919@gmail.com>
2021-01-31 14:58:56 +0600

    first commit

---
+++ word-reverse.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    gets(str);
+    wordReverse(str);
+    return 0;
+}
+
+void wordReverse(char str[])
+{
+    int len,i,j,count=0;
+    len = strlen(str);
+    for(i=0;i<len;i++)
+    {
+        if(str[i]==' ')
+        {
+            count++;
+        }
+    }
+    count=count+1;
+    char s[count][100];
+    int m=0,n=0;
+    for(i=0;i<=len;i++)
+    {
+        if(str[i]!=' ' && str[i]!='\0')
+        {
+            s[m][n]=str[i];
+            n++;
+        }
+        else
+        {
+            s[m][n]='\0';
+            m++;
+            n=0;
+        }
+    }
+    for(i=count-1;i>=0;i--)
+    {
+        j=0;
+        while (s[i][j]!='\0')
+        {
+            printf("%c",s[i][j]);
+            j++;
+        }
+        printf(" ");
+    }
+}
